5d922264e307516b3960df4eaaea07f90f0d68e1
Cyrus-mahle <67721071+Cyrus-mahle@users.noreply.github.com>
2022-03-05 11:23:47 +0200

    Add files via upload

---
+++ App.java
@@ -0,0 +1,20 @@
+public class App{
+
+public static void main(String[] args){
+
+
+String firstName="Mahle";
+int studentNumber=219293387;
+float balance=34.8883f;
+boolean isEmployed=false;
+char rank='A';
+
+System.out.println("Name : "+firstName);
+System.out.println("Student number : "+studentNumber);
+System.out.println("Balance : R"+balance);
+System.out.println("Employed : "+isEmployed);
+System.out.println("Rank : "+rank);
+
+  }
+
+}
